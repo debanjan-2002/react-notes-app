30d4f5bd6251de689058f3132d337b8c5005ff53
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-06 21:56:09 +0530

    Made Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return <div className={styles.header_container}>Header</div>;
+};
+
+export default Header;
