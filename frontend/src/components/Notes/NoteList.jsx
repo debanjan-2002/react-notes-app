2657367f73236542c3268ec401a7ac3e9755a0a3
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-06 21:54:17 +0530

    Made NoteList component (with styles)

---
+++ frontend/src/components/Notes/NoteList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import NoteItem from "./NoteItem";
+import styles from "./NoteList.module.css";
+
+const NoteList = props => {
+    return (
+        <div className={styles.note_list}>
+            {props.notes.map(note => (
+                <NoteItem
+                    id={note._id}
+                    key={note._id}
+                    lastModified={note.lastModified.toLocaleString()}
+                    title={note.title}
+                    description={note.description}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default NoteList;
