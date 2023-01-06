f920e35b119d6a3e725e70985c10d908fd4ecb4d
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-07 00:54:14 +0530

    Added logic to receive new note data from frontend and saving in the database

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 mongoose.set("strictQuery", true);
 
 const Note = require("./models/notes.js");
@@ -16,11 +17,23 @@ mongoose
     .catch(err => console.log("Error connecting to MongoDB: " + err.message));
 
 // DONE: Fetching all the notes from MongoDB
-app.get("/notes", async (req, res) => {
+app.get("/api/notes", async (req, res) => {
     // This returns all the notes as an array of objects
     const notes = await Note.find({});
     // Sending the notes from the server to the frontend
     res.json(notes);
+});
+
+// DONE: Receiving the new note body sent from the frontend and saving it in the database
+app.post("/api/notes", async (req, res) => {
+    // Destructuring the title and description of the note from the request body
+    const { title, description } = req.body;
+    // Making a new note instance with the received title and description
+    const note = new Note({ title, description });
+    // Saving the new note in the database
+    await note.save();
+    // Sending the inserted note to the frontend
+    res.json(note);
 });
 
 // Setting up PORT
