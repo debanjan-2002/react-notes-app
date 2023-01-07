4b8bb3ae5a25c1c25a6bc88710889c19faaef8ea
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-07 18:13:21 +0530

    Made delete route to delete note from database

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/api/notes", async (req, res) => {
     // This returns all the notes as an array of objects
     const notes = await Note.find({});
     // Sending the notes from the server to the frontend
-    res.json(notes);
+    res.status(200).json(notes);
 });
 
 // DONE: Receiving the new note body sent from the frontend and saving it in the database
@@ -33,7 +33,18 @@ app.post("/api/notes", async (req, res) => {
     // Saving the new note in the database
     await note.save();
     // Sending the inserted note to the frontend
-    res.json(note);
+    res.status(200).json(note);
+});
+
+// DONE: Set up the route to delete a note
+app.delete("/api/notes", async (req, res) => {
+    // Destructuring the id of the note that is to be deleted from the database
+    // It is sent from the frontend
+    const { id } = req.body;
+    // Deleting the note with the _id : id from the database
+    const deletedNote = await Note.findByIdAndDelete(id);
+    // Sending the deleted note to the frontend
+    res.status(200).json(deletedNote);
 });
 
 // Setting up PORT
