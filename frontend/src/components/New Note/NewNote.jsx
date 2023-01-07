bd438df229a30f0d4f9834230de0e86c436da1d5
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-07 14:43:40 +0530

    Added state to manage opening and closing of new form

--- frontend/src/components/New Note/NewNote.jsx
+++ frontend/src/components/New Note/NewNote.jsx
@@ -1,53 +1,33 @@
 import React, { useState } from "react";
 
+import NewNoteForm from "./NewNoteForm";
 import styles from "./NewNote.module.css";
 
 const NewNote = props => {
-    const [formData, setFormData] = useState({
-        title: "",
-        description: ""
-    });
+    const [showNewForm, setShowNewForm] = useState(false);
 
-    const inputChangeHandler = e => {
-        setFormData(prevState => {
-            return { ...prevState, [e.target.name]: e.target.value };
-        });
+    const openNewFormHandler = () => {
+        setShowNewForm(true);
     };
 
-    const formSubmitHandler = e => {
-        e.preventDefault();
-
-        props.onSubmit(formData);
-        setFormData({ title: "", description: "" });
+    const closeNewFormHandler = () => {
+        setShowNewForm(false);
     };
 
     return (
-        <form className={styles.form_control} onSubmit={formSubmitHandler}>
-            <div className={styles.input_container}>
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    onChange={inputChangeHandler}
-                    value={formData.title}
+        <React.Fragment>
+            {!showNewForm && (
+                <div className={styles.button_container}>
+                    <button onClick={openNewFormHandler}>Add Note</button>
+                </div>
+            )}
+            {showNewForm && (
+                <NewNoteForm
+                    onSubmit={props.onSubmit}
+                    onClose={closeNewFormHandler}
                 />
-            </div>
-            <div className={styles.input_container}>
-                <label htmlFor="description">Description</label>
-                <textarea
-                    name="description"
-                    id="description"
-                    cols="30"
-                    rows="5"
-                    onChange={inputChangeHandler}
-                    value={formData.description}
-                ></textarea>
-            </div>
-            <div className={styles.button_container}>
-                <button type="submit">Add Note</button>
-            </div>
-        </form>
+            )}
+        </React.Fragment>
     );
 };
 
