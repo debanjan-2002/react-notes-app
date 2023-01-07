341130f236531b7942d2d99f8ef3e504462fd9a8
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-07 15:06:59 +0530

    Added comments

--- frontend/src/components/New Note/NewNote.jsx
+++ frontend/src/components/New Note/NewNote.jsx
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 import NewNoteForm from "./NewNoteForm";
 import styles from "./NewNote.module.css";
 
+// DONE: Toggling the new form visibility and sending the data to the parent component
 const NewNote = props => {
+    // This state is used to keep track of whether the form should be shown or hidden
     const [showNewForm, setShowNewForm] = useState(false);
 
+    // This method is set the state to true (to open the form)
     const openNewFormHandler = () => {
         setShowNewForm(true);
     };
 
+    // This method is set the state to false (to close the form)
     const closeNewFormHandler = () => {
         setShowNewForm(false);
     };
--- frontend/src/components/New Note/NewNoteForm.jsx
+++ frontend/src/components/New Note/NewNoteForm.jsx
@@ -2,22 +2,32 @@ import React, { useState } from "react";
 
 import styles from "./NewNoteForm.module.css";
 
+// DONE: Taking the data from the form and sending it to the parent component
 const NewNoteForm = props => {
+    // formData state is used to keep track of the input & textarea changes
+    // It is initialized with empty title and description
     const [formData, setFormData] = useState({
         title: "",
         description: ""
     });
 
+    // This method is called everytime there is a change in the input or textarea
     const inputChangeHandler = e => {
+        // If there is change (due to which is method gets called), we update the state with the new value
         setFormData(prevState => {
             return { ...prevState, [e.target.name]: e.target.value };
         });
     };
 
+    // This method is called when the form is submitted
     const formSubmitHandler = e => {
+        // This statement is required to stop the default behaviour of form submission
+        // It prevents the page from refreshing
         e.preventDefault();
 
+        // We pass the stored data from the formData state to the parent component for futher processing
         props.onSubmit(formData);
+        // After the data is sent to the parent component, we reset (clear) the input and textarea
         setFormData({ title: "", description: "" });
     };
 
