36de9f6503bd916eb83e5433e0a275cb67484f3b
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-07 14:42:35 +0530

    Refactored the new note form into a NewNoteForm component

---
+++ frontend/src/components/New Note/NewNoteForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import styles from "./NewNoteForm.module.css";
+
+const NewNoteForm = props => {
+    const [formData, setFormData] = useState({
+        title: "",
+        description: ""
+    });
+
+    const inputChangeHandler = e => {
+        setFormData(prevState => {
+            return { ...prevState, [e.target.name]: e.target.value };
+        });
+    };
+
+    const formSubmitHandler = e => {
+        e.preventDefault();
+
+        props.onSubmit(formData);
+        setFormData({ title: "", description: "" });
+    };
+
+    return (
+        <form className={styles.form_control} onSubmit={formSubmitHandler}>
+            <div className={styles.input_container}>
+                <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    onChange={inputChangeHandler}
+                    value={formData.title}
+                />
+            </div>
+            <div className={styles.input_container}>
+                <label htmlFor="description">Description</label>
+                <textarea
+                    name="description"
+                    id="description"
+                    cols="30"
+                    rows="5"
+                    onChange={inputChangeHandler}
+                    value={formData.description}
+                ></textarea>
+            </div>
+            <div className={styles.button_container}>
+                <button type="submit">Add Note</button>
+                <button type="button" onClick={props.onClose}>
+                    Cancel
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default NewNoteForm;
